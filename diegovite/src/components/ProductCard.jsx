c9bc62823d826ec07bbc56284496d69a182d8776
diegoortzsrn <diegootzsrn@gmail.com>
2025-12-31 12:57:06 -0600

    Update ProductCard to use WhatsApp for orders

--- diegovite/src/components/ProductCard.jsx
+++ diegovite/src/components/ProductCard.jsx
@@ -2,15 +2,17 @@ function ProductCard({ product, onAdd }) {
   return (
     <div className="card">
       <h3>{product.name}</h3>
-      <p>${product.price}</p>
+      <br></br>
+      <b><p> Precio :   ${product.price}</p></b>
+      <br></br>
+      <b><p>Disponibles:  {product.stock}</p></b>
+
 
       <br></br><br></br>
-      <button className="btn" onClick={() => onAdd(product)}>
+      <button className="btn" onClick={() => window.open(`https://wa.me/50379106927?text=Hola,%20quisiera%20pedir%20${encodeURI(product.name)}`, '_blank')}>
         
-        Agregar
+        Pedir a WhatsApp
       </button>
     </div>
   );
 }
-
-export default ProductCard;
